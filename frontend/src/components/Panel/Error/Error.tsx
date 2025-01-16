47191137bb77031ac2d9de43a8bffc5f71b79d8b
Meow <meow@gemdzqq.com>
2025-01-16 19:31:19 +0800

    feat: add Error component

---
+++ frontend/src/components/Panel/Error/Error.tsx
@@ -0,0 +1,11 @@
+
+
+const ErrorComponent = () => {
+    return (
+        <div className={"w-auto h-auto bg-[#FFF2F0] text-center"} >
+            <h2>Error Occurred😭</h2>
+        </div>
+    );
+};
+
+export default ErrorComponent;
