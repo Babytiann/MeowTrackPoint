0688b87b2b6c057a57b165c3366a9418619bc4bb
Meow <meow@gemdzqq.com>
2025-01-16 19:10:36 +0800

    feat: SDK done

--- frontend/src/SDK/StatisticSDK.ts
+++ frontend/src/SDK/StatisticSDK.ts
@@ -3,17 +3,26 @@ import  axios from "axios";
 declare type query ={
     uuid?: string;
     event?: string;
+    time?: PerformanceEntry[];
+    message?: string;
+    stack?: string;
+    type?: string;
 }
+/*路由： /timing 接收时间
 
+        /demo  接收用户行为数据
+*/
 class StatisticSDK{
     uuid: string;
     navigationEntries = performance.getEntriesByType('navigation');
 
     constructor(UUID: string) {
         this.uuid = UUID;
-    }
+        this.initPerformance()
+        this.initError()
+    };
 
-    send(Url: string, query: query= {}){
+    send(Url: string, query:query = {}){
         query.uuid = this.uuid;                             //添加事件名称
 
         axios({
@@ -33,6 +42,7 @@ class StatisticSDK{
             })
     }
 
+    //事件监控
     event(key: string, val = {}){
         const eventURL = '/demo';
         this.send(eventURL,{event:key, ...val});
@@ -46,6 +56,36 @@ class StatisticSDK{
         this.event("uv", {uuid: this.uuid})
     }
 
+    //性能监控
+    initPerformance(){
+        if (this.navigationEntries.length > 0){
+            const url:string = '/timing';
+            this.send(url,{time: this.navigationEntries});
+        }
+    };
+
+    //错误处理
+    error(err: Error, etraInfo:Record<string, unknown> = {}) {
+        const errorURL = '/error';
+        const { message, stack } = err;
+
+        const transform = {
+            message: String(message),
+            stack: String(stack),
+            ...etraInfo // 展开 etraInfo 对象
+        };
+
+        this.send(errorURL, {...transform})
+    }
+
+    initError(){
+        window.addEventListener('error', event=>{
+            this.error(event.error);
+        })
+        window.addEventListener('unhandledrejection', event=>{
+            this.error(new Error(event.reason), { type: 'unhandledrejection'})
+        })
+    }
 
 }
 
