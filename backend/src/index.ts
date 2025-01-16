ffc7ba97370eb925713b7a1884dadc0d5b19fc28
Meow <meow@gemdzqq.com>
2025-01-16 14:32:20 +0800

    feat: add router && testPage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,33 @@
 import express from 'express';
+import book from './router/test'
+import cors from 'cors'
 
-const app = express()
+const app = express();
+app.use('/book', book)
+
 const port = 5927
+// 中间件 1：打印请求日志
+app.use((req, res, next) => {
+    console.log(`Request made to: ${req.url}`);
+    next();  // 调用下一个中间件
+});
+
+// 中间件 2：添加自定义头部
+app.use((req, res, next) => {
+    res.header("X-Custom-Header", "CustomValue");
+    console.log(`Next middle`);
+    next();  // 调用下一个中间件
+});
+
+app.use(cors()); //返回如果是
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.get('/baby', (req, res) => {
+    res.status(200).json('route baby')
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
---
+++ backend/src/router/test.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router
+    .get("/", (req: Request, res: Response) => {
+        res.send("Get a random book");
+    })
+    .post("/", (req: Request, res: Response) => {
+        res.send("Add a book");
+    })
+    .put("/", (req: Request, res: Response) => {
+        res.send("Update the book");
+    });
+
+export default router;
