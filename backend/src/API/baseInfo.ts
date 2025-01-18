a052f7a3f34f8de44ba3f50ad0db384ca5e32d8a
Meow <meow@gemdzqq.com>
2025-01-18 16:44:04 +0800

    refactor: change response status

--- backend/src/API/baseInfo.ts
+++ backend/src/API/baseInfo.ts
@@ -74,7 +74,7 @@ router
         try{
             const data = req.body;
             await insertData(data);
-            res.status(201).json({message: "Data send to baseInfo successfully"});
+            res.status(200).json({message: "Data send to baseInfo successfully"});
         }catch (error){
             console.error("Data send unsuccessfully", error);
             res.status(500).json({ message: "Error inserting data" });
