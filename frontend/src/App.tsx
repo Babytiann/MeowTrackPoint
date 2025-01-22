f14d85959a0aa2ab6d6f0cf2c39b4097da55cbc1
Meow <meow@gemdzqq.com>
2025-01-22 16:45:29 +0800

    🎉feat: finish basic navigator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import {Route, Routes} from "react-router";
 
 import Page from './components/testPage/Main.tsx';
-import BaseArea from "./components/Panel/Base.tsx";
+import Base from "./components/Panel/Base.tsx";
+import Home from "./components/Panel/components/Home.tsx";
 
 function App() {
   return (
       <Routes>
-          <Route path={"/"} element={<BaseArea />}/>
+          <Route path={"/"} element={<Base />}>
+              <Route index element={<Home />}></Route>
+              <Route path={"/userlist"} element={<Home />}></Route>  {/*暂且这么写*/}
+          </Route>
           <Route path={"/page"} element={<Page />} />
       </Routes>
   )
--- frontend/src/components/ErrorPage/Error.tsx
+++ frontend/src/components/ErrorPage/Error.tsx
@@ -1,6 +1,6 @@
 const ErrorComponent = () => {
     return (
-        <div className={"w-auto h-auto bg-[#FFF2F0] text-center rounded-xl"} >
+        <div className="w-auto h-auto bg-[#FFF2F0] text-center rounded-xl" >
             <h2>Error Occurred😭</h2>
         </div>
     );
