a26c02c1a8eb03fe8184ad0cf7168b8a3af70d0c
Meow <meow@gemdzqq.com>
2025-01-26 17:56:32 +0800

    🎉feat: add dark:

--- frontend/src/components/Panel/components/Header.tsx
+++ frontend/src/components/Panel/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header(){
     return (
-        <div className="flex h-[8vh] bg-rose-200 rounded-lg">
+        <div className="flex h-[8vh] bg-rose-200 rounded-lg dark:bg-darkBg">
             111
         </div>
     )
