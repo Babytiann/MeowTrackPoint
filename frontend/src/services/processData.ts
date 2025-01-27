647263f223156c288dd04dae48f259ec6af77471
Meow <meow@gemdzqq.com>
2025-01-27 17:40:45 +0800

    🚧fix: Fixing the  error hours

--- frontend/src/services/processData.ts
+++ frontend/src/services/processData.ts
@@ -13,7 +13,10 @@ function processData(demoData: DemoData[], type: string = "") {
         // 只处理指定类型的事件
         if (item.event !== type) return;
 
-        const dateWithHour = new Date(item.create_at).toISOString().split('T')[0] + ' ' + new Date(item.create_at).getUTCHours().toString().padStart(2, '0');
+        const date = new Date(item.create_at);
+        date.setHours(date.getHours() + 8);  // 将时间加 8 小时，我也不知道哪里来的误差，反正加上就对了
+
+        const dateWithHour = date.toISOString().split('T')[0] + ' ' + date.getUTCHours().toString().padStart(2, '0');
 
         // 更新 pvData
         pvData[dateWithHour] = (pvData[dateWithHour] || 0) + 1;
