fe51fd08eb9dc754dd2156779ce6e5a0cb6dbfd2
Meow <meow@gemdzqq.com>
2025-01-27 13:09:26 +0800

    🔨refactor: support different event

--- frontend/src/SDK/processData.ts
+++ frontend/src/SDK/processData.ts
@@ -1,23 +1,26 @@
-/*
-* 此函数就是为了给PUV的两张图表提供PUV数据用的
-*/
-
 interface DemoData {
     uuid: string;
     create_at: string;
     event: string;
 }
 
-function processData(demoData: DemoData[]) {
+function processData(demoData: DemoData[], type: string) {
     const pvData: { [key: string]: number } = {};
     const uvData: { [key: string]: Set<string> } = {};
 
+    // 过滤出需要的事件
+
+
     demoData.forEach(item => {
-        if (item.event !== 'puv') return; // 只统计 puv 事件
+        // 只处理指定类型的事件
+        if (item.event !== type) return;
 
         const dateWithHour = new Date(item.create_at).toISOString().split('T')[0] + ' ' + new Date(item.create_at).getUTCHours().toString().padStart(2, '0');
 
+        // 更新 pvData
         pvData[dateWithHour] = (pvData[dateWithHour] || 0) + 1;
+
+        // 更新 uvData
         if (!uvData[dateWithHour]) uvData[dateWithHour] = new Set();
         uvData[dateWithHour].add(item.uuid);
     });
